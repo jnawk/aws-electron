6c5acbda6c9e65af022eb5711dfa9cf9cbf08657
Philip Dowie <philip@jnawk.nz>
2018-10-02 23:02:17 +1300

    layout sorted

--- app/AWSConsole.jsx
+++ app/AWSConsole.jsx
@@ -3,6 +3,8 @@ import {Container,Row,Col,Button} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 const remote = require('electron').remote;
 
+const roleRegex = /arn:aws:iam::(\d{12}):role\/(.*)/;
+
 const getAWSConfig = remote.require('./AWSConfigReader');
 const getConsoleURL = remote.require('./getConsoleURL');
 const launchConsole = profileName => {
@@ -42,21 +44,53 @@ class AWSConsole extends React.Component {
 
     render() {
         return <Container>
-            <Row>
-                <Col sm={3}>Profile Name</Col>
-                <Col className='d-none d-sm-block' sm={7}>Role ARN</Col>
-                <Col className='d-none d-sm-none d-md-block' sm={2}>MFA ARN or Serial Number</Col>
-                <Col className='d-none d-sm-none d-md-block' sm={2}>Credentials Profile</Col>
-            </Row>
-            {this.usableProfiles.map(profileName => <Row>
-                <Col sm={3}>{profileName}</Col>
-                <Col className='d-none d-sm-block' sm={7}>{this.awsConfig[profileName].role_arn}</Col>
-                <Col className='d-none d-sm-none d-md-block' sm={2}>{this.awsConfig[profileName].mfa_serial}</Col>
-                <Col className='d-none d-sm-none d-md-block' sm={2}>{this.awsConfig[profileName].source_profile}</Col>
-                <Col sm={2}>
-                    <Button onClick={() => launchConsole(profileName)}>Launch</Button>
+            <Row className='d-none d-sm-table-row'>
+                <Col className='d-none d-sm-table-cell' sm={2} md={3}>
+                    Profile Name
                 </Col>
-            </Row>)}
+                <Col className='d-none d-md-table-cell' md={3}>
+                    Role Account
+                </Col>
+                <Col className='d-none d-md-table-cell' md={2}>
+                    Role Name
+                </Col>
+                <Col className='d-none d-lg-table-cell' lg={2}>
+                    MFA ARN or Serial Number
+                </Col>
+                <Col className='d-none d-md-table-cell' md={2}>
+                    Credentials Profile
+                </Col>
+            </Row>
+            {this.usableProfiles.map(profileName => {
+                const profile = this.awsConfig[profileName];
+                const roleRegexResult = roleRegex.exec(profile.role_arn);
+                return <Row className='d-table-row'>
+                    <Col className='d-none d-sm-table-cell' sm={2} md={3}>
+                        {profileName.replace(/-/g, String.fromCharCode(0x2011))}
+                    </Col>
+                    <Col className='d-none d-md-table-cell' md={3}>
+                        {roleRegexResult[1]}
+                    </Col>
+                    <Col className='d-none d-md-table-cell' md={2}>
+                        {roleRegexResult[2].replace(/-/g, String.fromCharCode(0x2011))}
+                    </Col>
+                    <Col className='d-none d-lg-table-cell' lg={2}>
+                        {profile.mfa_serial}
+                    </Col>
+                    <Col className='d-none d-md-table-cell' md={2}>
+                        {profile.source_profile.replace(/-/g, String.fromCharCode(0x2011))}
+                    </Col>
+                    <Col className='d-table-cell d-sm-none'>
+                        <Button onClick={() => launchConsole(profileName)}>
+                            {profileName}
+                        </Button>
+                    </Col>
+                    <Col className='d-none d-sm-table-cell' sm={2} md={2}>
+                        <Button onClick={() => launchConsole(profileName)}>
+                            Launch
+                        </Button>
+                    </Col>
+                </Row>;})}
         </Container>;
     }
 }
