5e5e1c16a4524efaa54563a43ff38a304fdf1dcb
Philip Dowie <philip@jnawk.nz>
2018-10-05 23:05:38 +1300

    added MFA to UI

--- app/AWSConsole.jsx
+++ app/AWSConsole.jsx
@@ -34,6 +34,10 @@ class AWSConsole extends React.Component {
         this.awsConfig = getAWSConfig();
         this.usableProfiles = Object.keys(this.awsConfig)
             .filter(key => Object.keys(this.awsConfig[key]).includes('role_arn'));
+
+        this.state = {
+            mfaCode: ''
+        };
     }
 
     componentDidMount() {
@@ -64,6 +68,21 @@ class AWSConsole extends React.Component {
             {this.usableProfiles.map(profileName => {
                 const profile = this.awsConfig[profileName];
                 const roleRegexResult = roleRegex.exec(profile.role_arn);
+                const shouldDisable = profile.mfa_serial != undefined && this.state.mfaCode.length != 6;
+                const launchProfile = () => launchConsole(profileName);
+
+                const profileLaunchButton = <Button onClick={launchProfile}
+                    disabled={shouldDisable}
+                    title={shouldDisable ? 'Enter your 6-digit MFA code first!' : null}>
+                    {profileName}
+                </Button>;
+
+                const launchButton = <Button onClick={launchProfile}
+                    disabled={shouldDisable}
+                    title={shouldDisable ? 'Enter your 6-digit MFA code first!' : null}>
+                    Launch
+                </Button>;
+
                 return <Row className='d-table-row'>
                     <Col className='d-none d-sm-table-cell' sm={2} md={3}>
                         {profileName.replace(/-/g, String.fromCharCode(0x2011))}
@@ -81,16 +100,21 @@ class AWSConsole extends React.Component {
                         {profile.source_profile.replace(/-/g, String.fromCharCode(0x2011))}
                     </Col>
                     <Col className='d-table-cell d-sm-none'>
-                        <Button onClick={() => launchConsole(profileName)}>
-                            {profileName}
-                        </Button>
+                        {profileLaunchButton}
                     </Col>
                     <Col className='d-none d-sm-table-cell' sm={2} md={2}>
-                        <Button onClick={() => launchConsole(profileName)}>
-                            Launch
-                        </Button>
+                        {launchButton}
                     </Col>
-                </Row>;})}
+                </Row>;
+            })}
+            {this.usableProfiles.some(profile => this.awsConfig[profile].mfa_serial) ? <Row>
+                <Col>
+                    <input type='text'
+                        value={this.state.mfaCode}
+                        placeholder='MFA Code'
+                        onChange={event => this.setState({mfaCode: event.target.value})}/>
+                </Col>
+            </Row> : null}
         </Container>;
     }
 }
