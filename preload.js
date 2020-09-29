90a683b5f89b92a22e1213fc447cb6ae8ab4cbda
Philip Dowie <philip@jnawk.nz>
2020-09-29 22:59:12 +1300

    removed crap from preload

--- preload.js
+++ preload.js
@@ -1,6 +1,3 @@
 const { remote, ipcRenderer } = require('electron');
 window.getAWSConfig = remote.getGlobal('getAWSConfig');
 window.launchConsole = remote.getGlobal('launchConsole');
-window.ipcRenderer = ipcRenderer;
-
-window.TabGroupC = () => { return require('electron-tabs'); };
