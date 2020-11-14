eff1fddd32f02a02759b3dcc3429afa15aa2aaae
Philip Dowie <philip@jnawk.nz>
2020-11-14 22:58:26 +1300

    countdown timer initialization on window opening, not tab opening

--- tabs.js
+++ tabs.js
@@ -7,6 +7,26 @@ let myWindowNumber
 ipcRenderer.on("open-tab", (event, {url, tabNumber, windowNumber, profile, expiryTime}) => {
     if(windowNumber != undefined) {
         myWindowNumber = windowNumber
+        // set the expiry timer and message
+        const interval = setInterval(() => {
+            const currentTime = new Date().getTime()
+            const timeToGo = expiryTime - currentTime
+            if (timeToGo < 1000) {
+                clearInterval(interval)
+            } else {
+                const hoursRemaining = Math.floor(timeToGo / 3600000)
+                const minutesRemaining = Math.floor(
+                    (timeToGo - (hoursRemaining * 3600000)) / 60000
+                )
+                const secondsRemaining = Math.floor(
+                    (timeToGo - ((hoursRemaining * 3600000) + (minutesRemaining * 60000))) / 1000
+                )
+                const hoursMessage = hoursRemaining > 0 ? `${hoursRemaining} hours ` : ""
+                const minutesMessage = hoursRemaining > 0 && minutesRemaining > 0 ? `${minutesRemaining} minutes, ` : ""
+                const message = `Time remaining: ${hoursMessage}${minutesMessage}${secondsRemaining} seconds`
+                document.getElementById("timeRemaining").innerHTML = message
+            }
+        }, 1000)
     }
     tabGroup.addTab({
         title: "AWS Console",
@@ -30,23 +50,4 @@ ipcRenderer.on("open-tab", (event, {url, tabNumber, windowNumber, profile, expir
         }
     })
     ipcRenderer.send("add-tab", {windowNumber: myWindowNumber, tabNumber})
-    const interval = setInterval(() => {
-        const currentTime = new Date().getTime()
-        const timeToGo = expiryTime - currentTime
-        if (timeToGo < 1000) {
-            clearInterval(interval)
-        } else {
-            const hoursRemaining = Math.floor(timeToGo / 3600000)
-            const minutesRemaining = Math.floor(
-                (timeToGo - (hoursRemaining * 3600000)) / 60000
-            )
-            const secondsRemaining = Math.floor(
-                (timeToGo - ((hoursRemaining * 3600000) + (minutesRemaining * 60000))) / 1000
-            )
-            const hoursMessage = hoursRemaining > 0 ? `${hoursRemaining} hours ` : ""
-            const minutesMessage = hoursRemaining > 0 && minutesRemaining > 0 ? `${minutesRemaining} minutes, ` : ""
-            const message = `Time remaining: ${hoursMessage}${minutesMessage}${secondsRemaining} seconds`
-            document.getElementById("timeRemaining").innerHTML = message
-        }
-    }, 1000)
 })
