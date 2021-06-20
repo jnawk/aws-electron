85ecaab6a6099fde71ced40a5df8bbf8dee08285
Philip Dowie <philip@jnawk.nz>
2021-06-21 10:54:01 +1200

    lint

--- preload.js
+++ preload.js
@@ -2,5 +2,11 @@ const { ipcRenderer, contextBridge } = require("electron")
 
 contextBridge.exposeInMainWorld("backend", {
     getAWSConfig: () => ipcRenderer.invoke("get-aws-config"),
-    launchConsole: ({profileName, mfaCode, configType}) => ipcRenderer.send("launch-console", {profileName, mfaCode, configType})
+    launchConsole: ({profileName, mfaCode, configType}) => ipcRenderer.send(
+        "launch-console",
+        {profileName, mfaCode, configType}
+    ),
+    getUsableProfiles: ({config, credentialsProfiles}) => ipcRenderer.invoke(
+        "get-usable-profiles", {config, credentialsProfiles}
+    )
 })
