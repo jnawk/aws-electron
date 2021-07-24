94f8d95a361e2c834d16da3f1928630670b26493
Philip Dowie <philip@jnawk.nz>
2021-07-24 22:02:15 +1200

    lint

--- menu.js
+++ menu.js
@@ -1,91 +1,91 @@
-const { app, Menu } = require('electron')
+const { app, Menu } = require("electron")
 
-const isMac = process.platform === 'darwin'
+const isMac = process.platform === "darwin"
 
 const template = [
-  ...(isMac ? [{
-    label: app.name,
-    submenu: [
-      { role: 'about' },
-      { type: 'separator' },
-      { role: 'services' },
-      { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideothers' },
-      { role: 'unhide' },
-      { type: 'separator' },
-      { role: 'quit' }
-    ]
-  }] : []),
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Preferences',
-        click: async () => {
-          console.log("preferences to open...")
-        }
-      },
-      isMac ? { role: 'close' } : { role: 'quit' }
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      ...(isMac ? [
-        { role: 'pasteAndMatchStyle' },
-        { role: 'delete' },
-        { role: 'selectAll' },
-        { type: 'separator' },
-        {
-          label: 'Speech',
-          submenu: [
-            { role: 'startspeaking' },
-            { role: 'stopspeaking' }
-          ]
-        }
-      ] : [
-        { role: 'delete' },
-        { type: 'separator' },
-        { role: 'selectAll' }
-      ])
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      { role: 'reload' },
-      { role: 'forcereload' },
-      { role: 'toggledevtools' },
-      { type: 'separator' },
-      { role: 'resetzoom' },
-      { role: 'zoomin' },
-      { role: 'zoomout' },
-      { type: 'separator' },
-      { role: 'togglefullscreen' }
-    ]
-  },
-  {
-    label: 'Window',
-    submenu: [
-      { role: 'minimize' },
-      { role: 'zoom' },
-      ...(isMac ? [
-        { type: 'separator' },
-        { role: 'front' },
-        { type: 'separator' },
-        { role: 'window' }
-      ] : [
-        { role: 'close' }
-      ])
-    ]
-  },
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            { role: "about" },
+            { type: "separator" },
+            { role: "services" },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideothers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" }
+        ]
+    }] : []),
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "Preferences",
+                click: async () => {
+                    console.log("preferences to open...")
+                }
+            },
+            isMac ? { role: "close" } : { role: "quit" }
+        ]
+    },
+    {
+        label: "Edit",
+        submenu: [
+            { role: "undo" },
+            { role: "redo" },
+            { type: "separator" },
+            { role: "cut" },
+            { role: "copy" },
+            { role: "paste" },
+            ...(isMac ? [
+                { role: "pasteAndMatchStyle" },
+                { role: "delete" },
+                { role: "selectAll" },
+                { type: "separator" },
+                {
+                    label: "Speech",
+                    submenu: [
+                        { role: "startspeaking" },
+                        { role: "stopspeaking" }
+                    ]
+                }
+            ] : [
+                { role: "delete" },
+                { type: "separator" },
+                { role: "selectAll" }
+            ])
+        ]
+    },
+    {
+        label: "View",
+        submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { role: "toggledevtools" },
+            { type: "separator" },
+            { role: "resetzoom" },
+            { role: "zoomin" },
+            { role: "zoomout" },
+            { type: "separator" },
+            { role: "togglefullscreen" }
+        ]
+    },
+    {
+        label: "Window",
+        submenu: [
+            { role: "minimize" },
+            { role: "zoom" },
+            ...(isMac ? [
+                { type: "separator" },
+                { role: "front" },
+                { type: "separator" },
+                { role: "window" }
+            ] : [
+                { role: "close" }
+            ])
+        ]
+    },
 ]
 
 module.exports.appMenu = Menu.buildFromTemplate(template)
