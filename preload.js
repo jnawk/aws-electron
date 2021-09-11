0a19322b415e9d05d4f585a7cf810d39272fcb02
Philip Dowie <philip@jnawk.nz>
2021-09-12 00:48:25 +1200

    whitespace changes

--- preload.js
+++ preload.js
@@ -1,14 +1,19 @@
 const { ipcRenderer, contextBridge } = require("electron")
 
-contextBridge.exposeInMainWorld("backend", {
-    getAWSConfig: () => ipcRenderer.invoke("get-aws-config"),
-    launchConsole: ({profileName, mfaCode, configType}) => ipcRenderer.send(
-        "launch-console",
-        {profileName, mfaCode, configType}
-    ),
-    getUsableProfiles: ({config, credentialsProfiles}) => ipcRenderer.invoke(
-        "get-usable-profiles", {config, credentialsProfiles}
-    ),
-    getPreferences: () => ipcRenderer.invoke("get-preferences"),
-    setPreference: preference => ipcRenderer.send("set-preference", preference)
-})
+contextBridge.exposeInMainWorld(
+    "backend",
+    {
+        getAWSConfig: () => ipcRenderer.invoke("get-aws-config"),
+        launchConsole: ({profileName, mfaCode, configType}) => ipcRenderer.send(
+            "launch-console",
+            {profileName, mfaCode, configType}
+        ),
+        getUsableProfiles: ({config, credentialsProfiles}) => ipcRenderer.invoke(
+            "get-usable-profiles", {config, credentialsProfiles}
+        ),
+        getPreferences: () => ipcRenderer.invoke("get-preferences"),
+        setPreference: preference => ipcRenderer.send(
+            "set-preference", preference
+        ),
+    }
+)
