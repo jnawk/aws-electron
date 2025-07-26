51286797d6087d46604c0cc69a9acdb8c645932a
Philip Dowie <philip@jnawk.nz>
2025-07-26 13:19:49 +1200

    updated aws stuff

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -5,6 +5,11 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      rollupOptions: {
+        external: ["@aws-sdk/client-sso-oidc"],
+      },
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
